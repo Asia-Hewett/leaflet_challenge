c8484d35a4d839008e497df00174c05dcaafb4c3
Asia-Hewett <asia.hewett0046@gmail.com>
2020-10-29 16:14:40 -0500

    Adding First Map Layer

---
+++ static/js/logic.js
@@ -0,0 +1,17 @@
+// This is where we're creating the map
+
+let myMap = L.map("mapid", {
+    center: [38.8026, 116.4194], //not sure what it wants here
+    zoom: 7
+});
+
+// Add title layer
+
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
