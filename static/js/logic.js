60be5d1000e2fb615a99f5c10d77abb34fc0b8f7
Asia-Hewett <asia.hewett0046@gmail.com>
2020-10-30 18:34:27 -0500

    Creating Earthquake Maps

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,26 +1,6 @@
-// Testing
-// console.log(API_KEY);
-
-// This is where we're creating the map
-
-// Add title layer
-
-let variable = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
-});
-
-let myMap = L.map("mapid", {
-  center: [40, -94], 
-  zoom: 4 
-})
-variable.addTo(myMap);
-
+// This is where we're getting our data from
 let link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+let tectLink="https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
 
 // Create a function to generate the markers for earthquake magnitude
 
@@ -46,7 +26,67 @@ function markerColor(mag) {
 
 // Grab the geojson using d3
 
-d3.json(link, function(data) {
-  createFeatures(data.features);
-});
+  d3.json(link,function(data){
+    createFeatures(data.features);
+  });
 
+function createFeatures(earthquakeData) {
+
+  var earthquakes=L.geoJson(earthquakeData, {
+      pointToLayer: function(data, latlng) {
+        return L.circleMarker(latlng, {
+          radius: data.properties.mag,
+          color: markerColor(data.properties.mag),
+          opacity: 0.75,
+          fillOpacity: 0.75,
+          weight: 0
+        }).bindPopup("<h3>" + data.properties.place +
+        "</h3><hr><p>" + new Date(data.properties.time) + "</p>" + "<p>" +"Magnitude: "+data.properties.mag + "</p>");
+      }
+    });
+
+    // Adding earthquakes layer to the createMap function
+    createMap(earthquakes);
+    
+  }
+
+  function createMap(earthquakes) {
+  
+    // Define streetmap and darkmap layers
+    var grayscale = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?" +
+      "access_token=pk.eyJ1IjoibWV0YWxpY2FydXMiLCJhIjoiY2thN2V1bDBxMDJ5bTJ4bGo1a29temsxNCJ9.5DGFqjLK2yLYd9Uab-EyrQ");
+  
+    var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v10/tiles/256/{z}/{x}/{y}?" +
+    "access_token=pk.eyJ1IjoibWV0YWxpY2FydXMiLCJhIjoiY2thN2V1bDBxMDJ5bTJ4bGo1a29temsxNCJ9.5DGFqjLK2yLYd9Uab-EyrQ");
+  
+    var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
+    "access_token=pk.eyJ1IjoibWV0YWxpY2FydXMiLCJhIjoiY2thN2V1bDBxMDJ5bTJ4bGo1a29temsxNCJ9.5DGFqjLK2yLYd9Uab-EyrQ");
+    
+    var tectOutline=new L.LayerGroup();
+
+    d3.json(tectLink,function(data){
+        L.geoJson(data,{
+                color:"orange",
+                weight:2
+
+        }).addTo(tectOutline);
+        
+    });
+
+  // Add title layer
+
+  let landing = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  });
+
+  let myMap = L.map("mapid", {
+    center: [40, -94], 
+    zoom: 4 
+  })
+  landing.addTo(myMap);
+  }
