cd7c3b4f244c3ca8eac802417292993b967cbda3
Asia-Hewett <asia.hewett0046@gmail.com>
2020-10-30 16:57:19 -0500

    Created Magnitude Markers

--- static/js/logic.js
+++ static/js/logic.js
@@ -22,10 +22,31 @@ variable.addTo(myMap);
 
 let link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
+// Create a function to generate the markers for earthquake magnitude
+
+function markerSize(mag) {
+  return mag * 30000;
+}
+
+function markerColor(mag) {
+  if (mag <= 1) {
+      return "#ADFF2F";
+  } else if (mag <= 2) {
+      return "#9ACD32";
+  } else if (mag <= 3) {
+      return "#FFFF00";
+  } else if (mag <= 4) {
+      return "#ffd700";
+  } else if (mag <= 5) {
+      return "#FFA500";
+  } else {
+      return "#FF0000";
+  };
+}
+
 // Grab the geojson using d3
 
 d3.json(link, function(data) {
-  // Create a geojson layer with the retrieved data
-  L.geoJson(data).addTo(myMap);
+  createFeatures(data.features);
 });
 
